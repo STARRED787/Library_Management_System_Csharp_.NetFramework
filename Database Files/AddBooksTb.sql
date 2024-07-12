8bebe04f23d4c08cbcbfe5a9f2537e994c025ec3
STARRED787 <malithamalshan7@gmail.com>
2024-07-13 01:42:48 +0530

    C#

--- Database Files/AddBooksTb.sql
+++ Database Files/AddBooksTb.sql
@@ -1,9 +1,11 @@
-﻿CREATE TABLE [dbo].[AddBookTB] (
-    [Book_Name]        VARCHAR (100) NOT NULL,
-    [Book_Author]      NCHAR (100)    NOT NULL,
-    [Book_Publication] VARCHAR (100) NOT NULL,
-    [Purchase_Date]    DATETIME      NOT NULL,
-    [Book_Price]       SMALLMONEY    NOT NULL,
-    [Book_Quantity]    INT           NOT NULL
+﻿CREATE TABLE [dbo].[AddBooksTB] (
+    [Book_Id]          INT             NOT NULL,
+    [Book_Name]        VARCHAR (100)   NOT NULL,
+    [Book_Author]      NVARCHAR (100)  NOT NULL,
+    [Book_Publication] VARCHAR (100)   NOT NULL,
+    [Purchase_Date]    DATETIME        NOT NULL,
+    [Book_Price]       DECIMAL (10, 2) NOT NULL,
+    [Book_Quantity]    INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Book_Id] ASC)
 );
 
