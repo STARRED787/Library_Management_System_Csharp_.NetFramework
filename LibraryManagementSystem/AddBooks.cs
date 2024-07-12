5c01aa4222ce7b71adfebdc3753a3e536d71da48
STARRED787 <malithamalshan7@gmail.com>
2024-07-12 14:43:16 +0530

    C#

--- LibraryManagementSystem/AddBooks.cs
+++ LibraryManagementSystem/AddBooks.cs
@@ -16,5 +16,10 @@ namespace LibraryManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
